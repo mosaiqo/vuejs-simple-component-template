c0b1063fd465269be775baa59bec8dcb747d94d9
Boudy de Geer <boudydegeer@mosaiqo.com>
2016-07-16 00:29:03 +0200

    Fix bug with lang support

--- template/Component.vue
+++ template/Component.vue
@@ -1,4 +1,4 @@
 <!-- The entry file for {{name}}  -->
 <template src="./template.html"></template>
-<script src="./index.{{scripts}}" lang="{{scripts}}"></script>
-<style src="./styles.{{styles}}" lang="{{styles}}" scoped></style>
+<script {{^scripts}}src="./index.js"{{/scripts}}{{#scripts}}src="./index.{{scripts}}"{{/scripts}} {{#scripts}}lang="{{scripts}}"{{/scripts}}></script>
+<style {{^styles}}src="./styles.css"{{/styles}} {{#styles}}src="./styles.{{styles}}"{{/styles}} {{#styles}}lang="{{styles}}"{{/styles}} scoped></style>
