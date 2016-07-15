09c2bf6ed766345edb1622db11a16d7c9990076f
Boudy de Geer <boudydegeer@mosaiqo.com>
2016-07-16 00:55:53 +0200

    Fix error

--- template/Component.vue
+++ template/Component.vue
@@ -1,6 +1,6 @@
 <!-- The entry file for {{name}}  -->
 <template src="./template.html"></template>
 <!-- {{script}} -->
-<script {{#script}}src="./index.{{./script}}"{{/script}}{{^script}}src="./index.js"{{/script}} {{#script}}lang="{{./script}}}"{{/script}}></script>
+<script {{#script}}src="./index.{{./script}}"{{/script}}{{^script}}src="./index.js"{{/script}} {{#script}}lang="{{./script}}"{{/script}}></script>
 <!-- {{style}} -->
 <style {{#style}}src="./styles.{{./style}}"{{/style}}{{^style}}src="./styles.css"{{/style}} {{#style}}lang="{{./style}}"{{/style}} scoped></style>
