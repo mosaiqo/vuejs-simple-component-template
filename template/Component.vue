4426a56a42705d66b28554446e74460f9c462caf
Boudy de Geer <boudydegeer@mosaiqo.com>
2016-07-16 00:46:23 +0200

    Change style and script tags

--- template/Component.vue
+++ template/Component.vue
@@ -1,4 +1,6 @@
 <!-- The entry file for {{name}}  -->
 <template src="./template.html"></template>
-<script {{#scripts}}src="./index.{{scripts}}"{{/scripts}}{{^scripts}}src="./index.js"{{/scripts}} {{#scripts}}lang="{{scripts}}"{{/scripts}}></script>
-<style {{#styles}}src="./styles.{{styles}}"{{/styles}}{{^styles}}src="./styles.css"{{/styles}} {{#styles}}lang="{{styles}}"{{/styles}} scoped></style>
+<!-- {{script}} -->
+<script {{#script}}src="./index.{{script}}"{{/script}}{{^script}}src="./index.js"{{/script}} {{#script}}lang="{{script}}"{{/script}}></script>
+<!-- {{style}} -->
+<style {{#style}}src="./styles.{{style}}"{{/style}}{{^style}}src="./styles.css"{{/style}} {{#style}}lang="{{style}}"{{/style}} scoped></style>
