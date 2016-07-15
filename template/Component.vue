f1651a1a4cde02b5684462e81f0de07e3548cd15
Boudy de Geer <boudydegeer@mosaiqo.com>
2016-07-16 00:40:37 +0200

    Fix bug with else in mustache

--- template/Component.vue
+++ template/Component.vue
@@ -1,4 +1,4 @@
 <!-- The entry file for {{name}}  -->
 <template src="./template.html"></template>
-<script {{^scripts}}src="./index.js"{{/scripts}}{{#scripts}}src="./index.{{scripts}}"{{/scripts}} {{#scripts}}lang="{{scripts}}"{{/scripts}}></script>
-<style {{^styles}}src="./styles.css"{{/styles}} {{#styles}}src="./styles.{{styles}}"{{/styles}} {{#styles}}lang="{{styles}}"{{/styles}} scoped></style>
+<script {{#scripts}}src="./index.{{scripts}}"{{/scripts}}{{^scripts}}src="./index.js"{{/scripts}} {{#scripts}}lang="{{scripts}}"{{/scripts}}></script>
+<style {{#styles}}src="./styles.{{styles}}"{{/styles}}{{^styles}}src="./styles.css"{{/styles}} {{#styles}}lang="{{styles}}"{{/styles}} scoped></style>
